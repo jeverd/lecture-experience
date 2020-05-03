91ca8eb7923f8faa63fe89223aa2d8ca8e8e926d
Lucas Nogueira <lnogueir@uwaterloo.ca>
2020-05-02 22:32:47 -0400

    show jawad stuff

--- src/servers.js
+++ src/servers.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 const socketio = require('socket.io');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public/js'));
 app.use(express.static('public/css'));
 app.use(express.static('public/images'));
+app.use(bodyParser.json());
 app.use(helmet());
 const expressServer = app.listen(8080);
 const io = socketio(expressServer);
--- src/subscriptions.js
+++ src/subscriptions.js
@@ -3,3 +3,7 @@ const io = require('./servers.js').io;
 io.sockets.on('connection', socket => {
     console.log(`socket ${socket.id} connected`)
 })
+
+io.of('/guest').on('connection', socket => {
+    console.log(`socket ${socket.id} connected as guest`)
+})
