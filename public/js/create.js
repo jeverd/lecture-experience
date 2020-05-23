8edb0946b5b32db110a9b212315d8d0f5dc93a53
Sultan  Emaish <sultanemaish@Sultans-MacBook-Pro.local>
2020-05-22 23:38:09 -0400

    created create page frontend

--- public/js/create.js
+++ public/js/create.js
@@ -3,7 +3,10 @@ let create_but = document.querySelector('#create-lecture');
 create_but.addEventListener('click', e => {
     e.preventDefault()
     var xhr = new XMLHttpRequest()
-    let lecture_name = 'Math Class'
+    let name = document.querySelector('#lectureName');
+    let email = document.querySelector('#email');
+    var lecture_name = name.value;
+    var lecture_email = email.value;
     xhr.open('POST', '/create', true)
     xhr.setRequestHeader('Content-Type', 'application/json')
     xhr.onreadystatechange = function () {
@@ -14,7 +17,39 @@ create_but.addEventListener('click', e => {
     }
     xhr.send(JSON.stringify({
         name: lecture_name,
+        email: lecture_email,
         time: new Date(),
-        size: 128
     }));
 });
+
+let create_but = document.querySelector('#get-email');
+
+create_but.addEventListener('click', e => {
+    e.preventDefault()
+    var xhr = new XMLHttpRequest()
+    let name = document.querySelector('#lectureName');
+    let email = document.querySelector('#email');
+    var lecture_name = name.value;
+    var lecture_email = email.value;
+    xhr.open('POST', '/email', true)
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.onreadystatechange = function () {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            const response = JSON.parse(this.response)
+            window.location = response.redirectUrl;
+        }
+    }
+    xhr.send(JSON.stringify({
+        managerId: "anything"
+    }));
+});
+
+let name = document.querySelector('#lectureName');
+
+name.onkeyup = function (e) {
+    if(name.value != ""){
+        create_but.removeAttribute("disabled");
+    } else {
+        create_but.setAttribute("disabled", "disabled");
+    }
+};
