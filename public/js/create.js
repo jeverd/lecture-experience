7687dcb60363b1f0070ec3761bcb27646f604b52
Lucas Nogueira <lnogueira@localhost.localdomain>
2020-05-24 02:16:19 -0400

    style fixes to create lecture page

--- public/js/create.js
+++ public/js/create.js
@@ -15,41 +15,10 @@ create_but.addEventListener('click', e => {
             window.location = response.redirectUrl;
         }
     }
+
     xhr.send(JSON.stringify({
         name: lecture_name,
         email: lecture_email,
         time: new Date(),
     }));
 });
-
-let create_but = document.querySelector('#get-email');
-
-create_but.addEventListener('click', e => {
-    e.preventDefault()
-    var xhr = new XMLHttpRequest()
-    let name = document.querySelector('#lectureName');
-    let email = document.querySelector('#email');
-    var lecture_name = name.value;
-    var lecture_email = email.value;
-    xhr.open('POST', '/email', true)
-    xhr.setRequestHeader('Content-Type', 'application/json')
-    xhr.onreadystatechange = function () {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            const response = JSON.parse(this.response)
-            window.location = response.redirectUrl;
-        }
-    }
-    xhr.send(JSON.stringify({
-        managerId: "anything"
-    }));
-});
-
-let name = document.querySelector('#lectureName');
-
-name.onkeyup = function (e) {
-    if(name.value != ""){
-        create_but.removeAttribute("disabled");
-    } else {
-        create_but.setAttribute("disabled", "disabled");
-    }
-};
