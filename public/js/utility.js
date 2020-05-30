ed390a61d71dfb7a5b1ce544f2c5df7e664bf3fb
Lucas Nogueira <lnogueira@localhost.localdomain>
2020-05-30 01:37:51 -0400

    Refactored code with linter

--- public/js/utility.js
+++ public/js/utility.js
@@ -1,18 +1,22 @@
+/* eslint-disable no-restricted-properties */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-absolute-path */
+/* eslint-disable-next-line import/no-unresolved */
 import Point from '/classes/point.js';
 
-export function getMouseCoordsOnCanvas(e, canvas){
-    let rect = canvas.getBoundingClientRect();
-    let x = Math.round(e.clientX - rect.left);
-    let y = Math.round(e.clientY - rect.top);
-    return new Point(x, y); //(x:x, y:y) previously
+export function getMouseCoordsOnCanvas(e, canvas) {
+  const rect = canvas.getBoundingClientRect();
+  const x = Math.round(e.clientX - rect.left);
+  const y = Math.round(e.clientY - rect.top);
+  return new Point(x, y); // (x:x, y:y) previously
 }
 
 // This will find the distance for the drawing of the circle in the canvas
-export function findDistance(point1, point2){ //coord1 ==> start, coord2 ==> finish, 
-    let exp1 = Math.pow(point2.x - point1.x, 2);
-    let exp2 = Math.pow(point2.y - point1.y, 2);
+export function findDistance(point1, point2) { // coord1 ==> start, coord2 ==> finish,
+  const exp1 = Math.pow(point2.x - point1.x, 2);
+  const exp2 = Math.pow(point2.y - point1.y, 2);
 
-    let distance = Math.sqrt(exp1 + exp2);
+  const distance = Math.sqrt(exp1 + exp2);
 
-    return distance;
+  return distance;
 }
