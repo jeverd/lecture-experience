7bc32d1d82a8182b3d6e89029650a525aaf3a0ce
Jawad <jawadbhimanii@gmail.com>
2020-06-07 16:34:40 -0400

    fixed more logs issue:

--- server/subscriptions.js
+++ server/subscriptions.js
@@ -78,7 +78,6 @@ io.sockets.on('connection', (socket) => {
       socket.on('disconnect', () => {
         logger.info(`SOCKET: Manager of room ${roomToJoin} disconnected`);
         managerObj.sockedId = null;
-        logger.info(socket.handshake.session.inRoom);
         if (socket.handshake.session.inRoom) delete socket.handshake.session.inRoom; socket.handshake.session.save();
         redisClient.hmset('managers', {
           [urlUuid]: JSON.stringify(managerObj),
