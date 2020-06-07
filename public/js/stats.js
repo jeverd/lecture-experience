21435dc5d003167f8ca3f0a95ae2c377b9322a4e
Sultan  Emaish <sultanemaish@Sultans-MacBook-Pro.local>
2020-06-06 22:58:04 -0400

    integrated the graph functionality with the real data

--- public/js/stats.js
+++ public/js/stats.js
@@ -18,11 +18,58 @@ function getSecondsBetweenTwoTimes(start, end) {
 }
 
 function buildGraph(statsObj) {
-  console.log(statsObj);
   const timeTracks = statsObj.userTracker.map((obj) => obj.time);
+  const watchersTracks = statsObj.userTracker.map((obj) => obj.numberOfUser);
   const lectureDurationInSeconds = getSecondsBetweenTwoTimes(new Date(timeTracks[0]),
     new Date(timeTracks[timeTracks.length - 1]));
-  console.log(lectureDurationInSeconds);
+
+  // const lectureDurationInMinutes = Math.ceil(lectureDurationInSeconds / 60);
+  const timeIntervalInSeconds = lectureDurationInSeconds / 10;
+  const watchers = Array(10).fill(0);
+  const average = Array(10).fill(0);
+  const time = Array(10).fill(0);
+
+  for (let i = 0; i < timeTracks.length; i++) {
+    time[i] = getSecondsBetweenTwoTimes(new Date(timeTracks[0]),
+      new Date(timeTracks[i]));
+  }
+
+  for (let i = 1; i < watchersTracks.length - 1; i++) {
+    const index = Math.floor(time[i] / timeIntervalInSeconds);
+
+    if (index <= (i)) {
+      watchers[index] += watchersTracks[i];
+      average[index] += 1;
+    }
+  }
+
+  const averageWatchers = [];
+
+  for (let i = 0; i < 10; i++) {
+    if (watchers[i] === 0) {
+      if (i === 0) {
+        averageWatchers[i] = 0;
+      } else if (average[i] === 0) {
+        averageWatchers[i] = averageWatchers[i - 1];
+      } else {
+        averageWatchers[i] = 0;
+      }
+    } else {
+      averageWatchers[i] = Math.ceil(watchers[i] / average[i]);
+    }
+  }
+
+  const elem = document.getElementsByClassName('myBar');
+
+  const maxStudents = Math.max.apply(null, averageWatchers);
+
+  for (let i = 0; i < 10; i++) {
+    if (averageWatchers[i] === 0) {
+      elem[i].style.height = `${1}%`;
+    } else {
+      elem[i].style.height = `${(averageWatchers[i] / maxStudents) * 100}%`;
+    }
+  }
 }
 
 fetch(`/lecture/stats/${urlId}`, requestOpts)
@@ -34,139 +81,3 @@ fetch(`/lecture/stats/${urlId}`, requestOpts)
     }
     // else display error loading stats
   });
-
-const products = [
-  {
-    time: 1,
-    numStudent: 3,
-  },
-  {
-    time: 2,
-    numStudent: 4,
-  },
-  {
-    time: 3,
-    numStudent: 6,
-  },
-  {
-    time: 4,
-    numStudent: 7,
-  },
-  {
-    time: 5,
-    numStudent: 9,
-  },
-  {
-    time: 6,
-    numStudent: 8,
-  },
-  {
-    time: 7,
-    numStudent: 10,
-  },
-  {
-    time: 8,
-    numStudent: 14,
-  },
-  {
-    time: 9,
-    numStudent: 13,
-  },
-  {
-    time: 10,
-    numStudent: 14,
-  },
-  {
-    time: 11,
-    numStudent: 15,
-  },
-  {
-    time: 15,
-    numStudent: 14,
-  },
-  {
-    time: 16,
-    numStudent: 13,
-  },
-  {
-    time: 19,
-    numStudent: 15,
-  },
-  {
-    time: 20,
-    numStudent: 16,
-  },
-  {
-    time: 21,
-    numStudent: 17,
-  },
-  {
-    time: 22,
-    numStudent: 18,
-  },
-  {
-    time: 23,
-    numStudent: 19,
-  },
-  {
-    time: 25,
-    numStudent: 18,
-  },
-  {
-    time: 27,
-    numStudent: 0,
-  },
-  {
-    time: 28,
-    numStudent: 0,
-  },
-  {
-    time: 30,
-    numStudent: 0,
-  },
-];
-
-const finalTime = products[products.length - 1].time;
-
-const timeInterval = finalTime / 10;
-
-const watchers = Array(10).fill(0);
-const average = Array(10).fill(0);
-
-for (var i = 0; i < products.length; i++) {
-  const index = Math.floor(products[i].time / timeInterval);
-
-  if (index <= (i + 1)) {
-    watchers[index] += products[i].numStudent;
-    average[index] += 1;
-  }
-}
-
-console.log(watchers);
-console.log(average);
-
-const wat = [];
-
-for (var i = 0; i < 10; i++) {
-  if (watchers[i] === 0) {
-    if (i == 0) {
-      wat[i] = 0;
-    } else if (average[i] === 0) {
-      wat[i] = wat[i - 1];
-    } else {
-      wat[i] = 0;
-    }
-  } else {
-    wat[i] = Math.ceil(watchers[i] / average[i]);
-  }
-}
-
-const elem = document.getElementsByClassName('myBar');
-
-const maxStudents = Math.max.apply(null, wat);
-
-console.log(maxStudents);
-
-for (let k = 0; k < 10; k++) {
-  elem[k].style.height = `${(wat[k] / maxStudents) * 100}%`;
-}
