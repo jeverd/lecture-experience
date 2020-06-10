200fb3c61adadc314f6f4b1e5476ce9a98c7fb18
Lucas Nogueira <lnogueir@uwaterloo.ca>
2020-06-10 00:06:40 -0400

    refactored common code and added duplicate functionality

--- public/js/utility.js
+++ public/js/utility.js
@@ -25,6 +25,30 @@ export function findDistance(point1, point2) { // coord1 ==> start, coord2 ==> f
 export function dragifyImage(imgElem) {
   imgElem.classList.add('selected-area-img');
   imgElem.draggable = true;
+  imgElem.oncontextmenu = (ev) => {
+    ev.preventDefault();
+    console.log('I right clicked');
+  };
   imgElem.ondragstart = (ev) => ev.dataTransfer.setDragImage(ev.target, 10, 10);
   document.getElementsByTagName('BODY')[0].appendChild(imgElem);
+}
+
+export function getImgElemFromImgData(imgData) {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = imgData.width;
+  canvas.height = imgData.height;
+  ctx.putImageData(imgData, 0, 0);
+  const imgElem = document.createElement('img');
+  imgElem.src = canvas.toDataURL();
+  return imgElem;
+}
+
+export function getImgDataFromImgElem(imgElem) {
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.width = imgElem.width;
+  canvas.height = imgElem.height;
+  context.drawImage(imgElem, 0, 0);
+  return context.getImageData(0, 0, imgElem.width, imgElem.height);
 }
