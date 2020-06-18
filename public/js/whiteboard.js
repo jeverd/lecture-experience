e12b4ef986313832d6477dbfdf8e919743bb7e50
Lucas Nogueira <lnogueir@uwaterloo.ca>
2020-06-18 00:25:34 -0400

    adjust padding on chat and made it collapsible

--- public/js/whiteboard.js
+++ public/js/whiteboard.js
@@ -136,46 +136,16 @@ window.onload = async () => {
         } else {
           appendMessage(`You: ${message}`);
           appendFile(newFile, newFile.type, newFile.name, 'sender');
-  
+
           // Need to send object with file URL, mime type, and message
           const reader = new FileReader();
           reader.readAsDataURL(newFile);
-          reader.onload = function (e) {
+          reader.onload = (e) => {
             socket.emit('send-to-guests', room.lecture_details.id, message, e.target.result, newFile.type, newFile.name);
           };
         }
         messageInput.value = '';
         fileInput.value = '';
-      });
-
-      // On click for display messages button
-      document.querySelector('button#toggle-messages').addEventListener('click', (e) => {
-        e.preventDefault();
-        // If we want to include multiple separate chat windows, this is an easy way of doing that
-        const messagesChild = e.target.nextElementSibling;
-        e.target.classList.toggle('active-chat');
-        if (messagesChild.style.maxHeight) {
-          messagesChild.style.maxHeight = null;
-        } else if (messagesChild.scrollHeight >= 300) {
-          messagesChild.style.maxHeight = '300px';
-          messagesChild.style.overflow = 'scroll';
-        } else {
-          messagesChild.style.maxHeight = `${messagesChild.scrollHeight}px`;
-        }
-      });
-
-      // Refresh the chat window for the new message
-      document.querySelector('button#toggle-messages').addEventListener('redraw', (e) => {
-        e.preventDefault();
-
-        const messagesChild = e.target.nextElementSibling;
-        e.target.classList.add('active-chat');
-        if (messagesChild.scrollHeight >= 300) {
-          messagesChild.style.maxHeight = '300px';
-          messagesChild.style.overflow = 'scroll';
-        } else {
-          messagesChild.style.maxHeight = `${messagesChild.scrollHeight}px`;
-        }
       });
 
       document.querySelector('#end-lecture').addEventListener('click', () => {
@@ -328,19 +298,6 @@ window.onload = async () => {
         boards: whiteboard.boards,
         activeBoardIndex: whiteboard.currentBoard,
       });
-    }
-
-    function appendMessage(message) {
-      const messageElement = document.createElement('tr');
-      const tableData = document.createElement('td');
-      tableData.innerText = message;
-
-      messageElement.append(tableData);
-      messageContainer.append(messageElement);
-
-      const messageToggle = document.getElementById('toggle-messages');
-      const event = new Event('redraw');
-      messageToggle.dispatchEvent(event);
     }
   }
 };
