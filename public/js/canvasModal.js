da5ecc54ddc8281cab6b8bc75d41a1897da76e66
lukasmuller10 <lukasmullerdeoliveira@gmail.com>
2020-06-19 11:56:02 -0300

    added modal ui and fixed session issue

---
+++ public/js/canvasModal.js
@@ -0,0 +1,58 @@
+//Mic button
+document.getElementById('test-mic').addEventListener('click', () => {
+    const joinContent = document.getElementById('join-content');
+    const micContent = document.getElementById('mic-content');
+    joinContent.style.display ='none';
+    micContent.style.display ='block';
+});
+
+//back button
+document.getElementById('go-back').addEventListener('click', () => {
+    const joinContent = document.getElementById('join-content');
+    const micContent = document.getElementById('mic-content');
+    micContent.style.display ='none';
+    joinContent.style.display ='block';
+});
+
+
+//Start Select input
+document.querySelector('.custom-select-wrapper').addEventListener('click', function() {
+    this.querySelector('.custom-select').classList.toggle('open');
+})  
+
+for (const option of document.querySelectorAll(".custom-option")) {
+    option.addEventListener('click', function() {
+        if (!this.classList.contains('selected')) {
+            this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+            this.classList.add('selected');
+            this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+        }
+    })
+}
+window.addEventListener('click', function(e) {
+    const select = document.querySelector('.custom-select')
+    if (!select.contains(e.target)) {
+        select.classList.remove('open');
+    }
+});
+//End Select input
+
+//Start Volume Input//
+
+const
+    range = document.getElementById('range'),
+    rangeV = document.getElementById('rangeV'),
+    miccontent = document.getElementById('mic-content')
+    setValue = ()=>{
+        const
+            newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
+            newPosition = 10 - (newValue * 0.2);
+        rangeV.innerHTML = `<span>${range.value}</span>`;
+        rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
+    };
+document.addEventListener("DOMContentLoaded", setValue);
+range.addEventListener('input', setValue);
+miccontent.addEventListener('click',()=>{rangeV.style.display="none"});
+
+
+console.log('ola');
