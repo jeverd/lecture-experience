b2dd693106809b492a87e5ef8e28a70ae6b4c796
Lucas Nogueira <lnogueir@uwaterloo.ca>
2020-07-15 23:25:50 -0400

    style scrollbar

--- public/js/guest/guest.js
+++ public/js/guest/guest.js
@@ -92,11 +92,3 @@ window.onload = async () => {
     invalidNameDiv.style.opacity = 0;
   });
 };
-
-// document.querySelector('#scroll-up').addEventListener('click', () => {
-//   document.querySelector('.page-list').animate({ scrollTop: '-=100' }, 150);
-// });
-
-// document.querySelector('#scroll-down').addEventListener('click', () => {
-//   document.querySelector('.page-list').animate({ scrollTop: '+=100' }, 150);
-// });
