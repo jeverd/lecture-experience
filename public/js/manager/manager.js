934ee56364a1e467eed2116aa2bc4a5c3561320e
muller <lukasmullerdeoliveira@gmail.com>
2020-07-31 19:15:28 -0300

    modal click button

--- public/js/manager/manager.js
+++ public/js/manager/manager.js
@@ -88,27 +88,21 @@ window.onload = () => {
     });
 
     $('#modal-select-button').click(() => {
-      $('#welcome-lecture-modal').hide();
-      if (!hasWhiteboard) {
-        beginLecture(whiteboard, canvasStream);
-        $('#welcome-lecture-modal').hide();
-      } else {
-        fetch(`/validate/lecture?id=${roomId}`).then((req) => {
-          switch (req.status) {
-            case 200:
-              beginLecture(whiteboard, canvasStream);
-              $('#welcome-lecture-modal').hide();
-              break;
-            case 404:
-              window.location.replace('/error?code=1');
-              break;
-            case 401:
-              window.location.replace('/error?code=2');
-              break;
-            default: break;
-          }
-        });
-      }
+      fetch(`/validate/lecture?id=${roomId}`).then((req) => {
+        switch (req.status) {
+          case 200:
+            beginLecture(whiteboard, canvasStream);
+            $('#welcome-lecture-modal').hide();
+            break;
+          case 404:
+            window.location.replace('/error?code=1');
+            break;
+          case 401:
+            window.location.replace('/error?code=2');
+            break;
+          default: break;
+        }
+      });
     });
   });
 };
