439991b94b992ee457e6504153ef43fc9fc32745
Jawad <jawadbhimanii@gmail.com>
2020-08-02 19:53:36 -0400

    appspec [ci skip]

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,3 +3,4 @@ ls
 source lecture-experience/config/.env
 export $(cut -d= -f1 lecture-experience/config/.env)
 sudo -E docker-compose -f lecture-experience/docker/production.yml up -d
+sdfsdfsdfsdf
